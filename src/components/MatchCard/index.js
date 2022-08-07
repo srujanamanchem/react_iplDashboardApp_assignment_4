b219b365fa6eb8662d35fb394295edf5f55ee105
srujanamanchem <108615762+srujanamanchem@users.noreply.github.com>
2022-08-08 00:29:04 +0530

    second commit

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,1 +1,21 @@
-// Write your code here
+import './index.css'
+
+const MatchCard = props => {
+  const {matchCardDetails} = props
+  const {
+    result,
+    competingTeam,
+    competingTeamLogo,
+    matchStatus,
+  } = matchCardDetails
+  return (
+    <li className="team-match">
+      <img src={competingTeamLogo} alt={competingTeam} />
+      <h1 className="team-name">{competingTeam}</h1>
+      <p className="match-result">{result}</p>
+      <p className="match-status">{matchStatus}</p>
+    </li>
+  )
+}
+
+export default MatchCard
