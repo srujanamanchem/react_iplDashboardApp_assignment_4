e592c8ec97b007e76c18689eab91cd0af6be3b36
srujanamanchem <108615762+srujanamanchem@users.noreply.github.com>
2022-08-08 00:25:40 +0530

    second commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,60 @@
-// Write your code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import TeamCard from '../TeamCard'
+
+import './index.css'
+
+class Home extends Component {
+  state = {teamsList: [], isLoading: true}
+
+  componentDidMount() {
+    this.getTeamsList()
+  }
+
+  getTeamsList = async () => {
+    const response = await fetch('https://apis.ccbp.in/ipl')
+    const data = await response.json()
+
+    const updatedTeamsList = data.teams.map(each => ({
+      name: each.name,
+      id: each.id,
+      teamImageUrl: each.team_image_url,
+    }))
+    console.log(updatedTeamsList)
+
+    this.setState({teamsList: updatedTeamsList, isLoading: false})
+  }
+
+  render() {
+    const {isLoading, teamsList} = this.state
+
+    return (
+      <div className="app-container">
+        {isLoading ? (
+          <div testid="loader">
+            <Loader type="TailSpin" color="#ffffff" height={50} width={50} />
+          </div>
+        ) : (
+          <div className="ipl-container">
+            <div className="ipl-heading-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+                alt="ipl logo"
+                className="ipl-logo"
+              />
+              <h1 className="ipl-heading">IPL Dashboard</h1>
+            </div>
+            <ul className="teams-list">
+              {teamsList.map(eachTeam => (
+                <TeamCard key={eachTeam.id} eachTeam={eachTeam} />
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default Home
